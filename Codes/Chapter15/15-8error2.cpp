95f356af28df593a177c1dd09a49c43e2ada9f67
ZhangZhaoXiang <1536459718@qq.com>
2025-01-18 23:44:51 +0800

    chapter15

---
+++ Codes/Chapter15/15-8error2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cfloat>
+
+using namespace std;
+
+bool hmean(double a, double b, double * ans)
+{
+    if(a==-b)
+    {
+        * ans = DBL_MAX;
+        return false;
+    }
+    else
+    {
+        * ans = 2.0 * a * b / (a+b);
+        return true;
+    }
+}
+
+int main()
+{
+    double x, y, z;
+
+    cout<<"Enter two numbers: ";
+    while (cin>>x>>y)
+    {
+        if(hmean(x, y, &z))
+            cout<<"Harmonic mean of "<<x<<" and "<<y<<" is "<<z<<endl;
+        else
+            cout<<"One value must be the negative of the other. - try again.\n";
+        
+        cout<<"Enter next set of numbers <q to quit>: ";
+    }
+    cout<<"Bye!\n";
+    return 0;
+    
+}
